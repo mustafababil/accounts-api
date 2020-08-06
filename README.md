cd9472a1ef7c450d3c555cf880b5b9520c6bded1
Mustafa Babil <mustafababil@outlook.com>
2020-08-06 22:47:05 +0200

    Project setup: OpenAPI definition added. Swagger configured.

---
+++ README.md
@@ -0,0 +1,10 @@
+- Spring Boot 2.3.2
+- OpenAPI v3
+- Swagger 2
+- Lombok
+
+
+Generate API Controllers and Models
+```shell script
+./mvnw clean install
+```
---
+++ src/main/java/com/mybank/demo/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.mybank.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket demoApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.groupName("public-api")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mybank.demo"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DemoProject")
+                .description("DemoProject")
+                .contact(new Contact("Mustafa Babil", "https://github.com/mustafababil", ""))
+                .license("GPLv3")
+                .licenseUrl("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                .version("1.0")
+                .build();
+    }
+
+}
